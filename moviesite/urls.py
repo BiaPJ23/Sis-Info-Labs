125e5cbac5c698134b7a5b9daf0a73b7ce14b839
Bia <nsilva.beatriz@usp.br>
2024-09-23 16:15:32 -0300

    Criar site estático com Django

--- moviesite/urls.py
+++ moviesite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path # modificar esta linha
 
 urlpatterns = [
-    path('staticpages/', include('staticpages.urls')),
+    path('', include('staticpages.urls')),
     path('admin/', admin.site.urls),
 ]
